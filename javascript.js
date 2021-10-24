11cb5c5459ef5e0c26586dcb0edf9a23fc5f41fe
Jhanpool CUPE PEREZ <88795343+JhanpoolCupe@users.noreply.github.com>
2021-10-24 17:37:34 -0500

    proyecto
    restaurante

---
+++ javascript.js
@@ -0,0 +1,7 @@
+function colocar_titulo(id,titulo){
+    var elemento_lista= document.getElementById(id)
+    elemento_lista.textContent= titulo   
+}
+colocar_titulo("arrozconpollo","Arroz con Pollo")
+colocar_titulo("sopaseca","Sopa Seca")
+
